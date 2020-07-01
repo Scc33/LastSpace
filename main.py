67b4967c17f52848143a3d9029b14b336c7188cb
Sean Coughlin <seanmc4@illinois.edu>
2020-07-01 17:23:31 -0500

    Start drawing things onscreen

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import os
 import time
 import random
 
+# Load in fonts
+pygame.font.init()
+
+# Set window size
 WIDTH = 750
 HEIGHT = 750
 
@@ -24,4 +28,36 @@ BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
 YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
 
 # Background
-BACKGROUND = pygame.image.load(os.path.join("assets", "background-black.png"))
+BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
+
+def main():
+    run = True
+    FPS = 60
+    clock = pygame.time.Clock()
+
+    level = 1
+    lives = 5
+
+    main_font = pygame.font.SysFont("comicsans", 50)
+
+    def redraw_window():
+        WIN.blit(BACKGROUND, (0,0))
+
+        # Draw text
+        level_label = main_font.render(f"Level: {level}", 1, (255,255,255))
+        lives_label = main_font.render(f"Lives: {lives}", 1, (255,255,255))
+        WIN.blit(lives_label, (10,10))
+        WIN.blit(level_label, (WIDTH - level_label.get_width() - 10,10))
+
+        pygame.display.update()
+
+    while run:
+        clock.tick(FPS) # tick the clock 60 times per second
+        redraw_window()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+
+main()
