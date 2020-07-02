5ae43ad15d25595970b217b252f9e27f2a1ee49b
Sean Coughlin <seanmc4@illinois.edu>
2020-07-02 11:00:39 -0500

    Complete basic game

--- test.py
+++ test.py
@@ -8,13 +8,15 @@ pygame.init()
 WIDTH = 750
 HEIGHT = 750
 
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Last Space")
+screen = pygame.display
+screen.set_mode((720,480))
+#pygame.display.set_caption("Last Space")
 
-BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
-
-def redraw_window():
-    WIN.blit(BACKGROUND, (0,0))
-
-while True:
-    redraw_window()
+running = True
+while running: # This would start the event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: # This would be a quit event.
+            running = False # So the user can close the program
+    screen.fill(0,0,0) # This fills the screen with black colour.
+    pygame.display.flip() # This "flips" the display so that it shows something
+pygame.quit()
